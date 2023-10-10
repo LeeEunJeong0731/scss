d49ff3c2893c8d673a09c06a8c2c161cdc048bec
이은정 <leeeunjeong@ieunjeong-ui-MacBookAir.local>
2023-10-10 12:35:53 +0900

    html 생성 및 안에 포켓몬배열 리스트로 만들기

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const pokemon = require("pokemon");
-console.log(pokemon.all("ko"));
+// console.log(pokemon.all("ko"));
+const test = pokemon.all("ko");
+let HTMLLi = "<ul>" + "<li>" + [test] + "</li>" + "</ul>";
+
+let fs = require("fs");
+let data = HTMLLi;
+fs.writeFileSync("test.html", data);
+
+//? test 리턴 보니까 배열
